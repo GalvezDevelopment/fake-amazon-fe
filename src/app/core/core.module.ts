a0c1dfbb7b650da435fb15f505f700271cdc9123
Christian Aurelio Galvez Javalera <christiangalvez@Christians-MacBook-Air.local>
2024-02-21 23:09:59 -0700

    Palette created based on Amazon colors. Header component was started to be built which is using the recently palette created.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -11,7 +13,9 @@ import { FooterComponent } from './components/footer/footer.component';
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    SharedModule
   ],
   exports: [
     HeaderComponent,
