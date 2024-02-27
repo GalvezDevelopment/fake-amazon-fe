b2a0e1e472964c2e24ac5a864fc4c7595bdd2c6b
Christian Aurelio Galvez Javalera <christiangalvez@Christians-MacBook-Air.local>
2024-02-27 10:01:50 -0700

    Worked on two files only

--- src/app/cart/resume/resume.component.spec.ts
+++ src/app/cart/resume/resume.component.spec.ts
@@ -1,17 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 
 import { ResumeComponent } from './resume.component';
+import { AppState } from '../../store/store.module';
+import { cartProductsList } from '../../shared/utils/products.utils';
+import { By } from '@angular/platform-browser';
+import { SharedModule } from '../../shared/shared.module';
+import * as ProductSelectors from '../../store/cart/cart.selectors';
+
+const initialState: AppState = {
+  products: [],
+  cart: cartProductsList
+};
 
 describe('ResumeComponent', () => {
   let component: ResumeComponent;
   let fixture: ComponentFixture<ResumeComponent>;
+  let store: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ResumeComponent]
+      declarations: [ResumeComponent],
+      imports: [SharedModule],
+      providers: [
+        provideMockStore({ initialState })
+      ]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
+    store = TestBed.inject(MockStore);
+
     fixture = TestBed.createComponent(ResumeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +37,17 @@ describe('ResumeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show one product in the cart at least', () => {
+    const items = fixture.debugElement.queryAll(By.css('.cart-product'));
+    expect(items.length).toEqual(1);
+  });
+
+  it('should call the remeve function', () => {
+    const items = fixture.debugElement.query(By.css('#removeBtn'));
+    spyOn(component, 'remove');
+    items.nativeElement.click();
+    expect(component.remove).toHaveBeenCalled();
   });
 });
---
+++ src/app/shared/utils/products.utils.ts
@@ -0,0 +1,55 @@
+import { Product } from "../models/product";
+
+export const productsList: Product[] = [
+    {
+      "id": "B07CV4L6HX",
+      "title": "Green Leaf WW3D Wonder Extension Cord Winder, Gray, 150 Foot",
+      "imgUrl": "https://m.media-amazon.com/images/I/81cRe0AVC4L._AC_UL320_.jpg",
+      "productUrl": "https://www.amazon.ca/dp/B07CV4L6HX",
+      "stars": 4.4,
+      "reviews": 2876,
+      "price": 47.69,
+      "listPrice": 0,
+      "categoryName": "Industrial  Scientific",
+      "isBestSeller": false
+    },
+    {
+      "id": "B09N1HGY74",
+      "title": "8pcs Toilet Seat Bumpers Universal Toilet Replacement Bumper for Bidet Attachment Toilet Seat Bumper Pads with Strong Adhesive for Home, Hotel, Hospital",
+      "imgUrl": "https://m.media-amazon.com/images/I/512CpB-EP5L._AC_UL320_.jpg",
+      "productUrl": "https://www.amazon.ca/dp/B09N1HGY74",
+      "stars": 3.8,
+      "reviews": 55,
+      "price": 10.99,
+      "listPrice": 0,
+      "categoryName": "Industrial  Scientific",
+      "isBestSeller": false,
+    },
+    {
+      "id": "B087P7538J",
+      "title": "YaeCCC 19 Pcs Hole Saw Kit 3/4''(19mm)- 6''(152mm) Full Set in Case with Mandrels, Hex Key and Install Plate for Soft Wood, PVC Board and Plastic Plate Drilling",
+      "imgUrl": "https://m.media-amazon.com/images/I/71P+qLFDFcL._AC_UL320_.jpg",
+      "productUrl": "https://www.amazon.ca/dp/B087P7538J",
+      "stars": 4,
+      "reviews": 126,
+      "price": 25.99,
+      "listPrice": 27.99,
+      "categoryName": "Industrial  Scientific",
+      "isBestSeller": false
+    }
+  ];
+
+  export const cartProductsList: Product[] = [
+    {
+      "id": "B07CV4L6HX",
+      "title": "Green Leaf WW3D Wonder Extension Cord Winder, Gray, 150 Foot",
+      "imgUrl": "https://m.media-amazon.com/images/I/81cRe0AVC4L._AC_UL320_.jpg",
+      "productUrl": "https://www.amazon.ca/dp/B07CV4L6HX",
+      "stars": 4.4,
+      "reviews": 2876,
+      "price": 47.69,
+      "listPrice": 0,
+      "categoryName": "Industrial  Scientific",
+      "isBestSeller": false
+    }
+  ];
